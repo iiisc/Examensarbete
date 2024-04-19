f3ae5140c6449a28d95bf7a4776dde479712fcc7
iiisc <63256807+iiisc@users.noreply.github.com>
2024-04-19 11:45:10 +0200

    Adds support for multiple file uploads at once

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect
 from PyPDF2 import PdfReader
 
 UPLOAD_FOLDER = 'Uploads'
+ALLOWED_EXTENSIONS = {'pdf'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -16,21 +17,26 @@ def readPdf(file):
 def home():
    return render_template('index.html')
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
-        if 'file' not in request.files:
-            print('No file part')    
-            return redirect(request.url)
-        file = request.files['file']
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+        files = request.files.getlist("file")
+        for file in files:   
+            if file and allowed_file(file.filename):
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+                print("File uploaded succesfully")
+            else:
+                print("Filetype not allowed")
 
-        filepath = os.path.join("Uploads", file.filename)
-        extension = os.path.splitext(filepath)[-1]
-        
-        if extension == '.pdf':
-            return(readPdf(filepath))
-        
-        return("File uploaded")
+        return redirect(request.url)
+
+@app.route('/get_files', methods=['GET'])
+def get_files():
+    all_files = (os.listdir(os.path.join(app.config['UPLOAD_FOLDER'])))
+    return all_files
 
 if __name__ == '__main__':
     app.run()
