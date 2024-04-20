e1095e7b5b42d9fbd6f3b1ff0f1a734a5bba8cd5
viktoreandersson <95419638+viktoreandersson@users.noreply.github.com>
2024-04-20 23:19:45 +0200

    Fått ner felen till 3
    Felen är 3 pga att några jobbtitlar saknar attribut i testfil2 .

--- testavmodel.py
+++ testavmodel.py
@@ -3,11 +3,11 @@ import numpy as np
 import ast
 
 from fuzzywuzzy import process
-
+from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
-
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
@@ -88,7 +88,7 @@ for words in tfidf.vocabulary_:
     print(f"valda ord för tokenizer {words}") 
 print(f"Längd på lista av vocabluary {len(tfidf.vocabulary_)}")
 sgd = SGDClassifier()
-lr = LogisticRegression(solver='lbfgs')
+lr = LogisticRegression(solver='liblinear-')
 svc = LinearSVC()
 
 def j_score(y_true, y_pred):
@@ -101,46 +101,114 @@ def print_score(y_pred, clf):
   print('Jacard score: {}'.format(j_score(y_test, y_pred)))
   print('----')
 
-for classifier in [LinearSVC(C=99999999999999999999,penalty='l2', dual=False, multi_class="ovr", verbose=10)]:
+""" for classifier in [LinearSVC(C=999999,penalty='l2', dual=False, multi_class="ovr", verbose=10, fit_intercept=False)]:
   clf = OneVsRestClassifier(classifier)
+  LogisticRegression(solver='l2', dual=False,C=10000)
   clf.fit(X_train, y_train)
   y_pred = clf.predict(X_test)
-  print_score(y_pred, classifier)
+  print_score(y_pred, classifier) """
 
-for classifier in [LinearSVC(C=99999999999999999999,penalty='l2', dual=False)]:
+  
+solvers = ['liblinear', 'lbfgs', 'newton-cg', 'sag', 'saga']
+""" for solver in solvers:
+    clf = OneVsRestClassifier(LogisticRegression(solver=solver, max_iter=1000))
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(f"Solver: {solver}, Accuracy: {accuracy_score(y_test, y_pred)}")
+
+ """
+
+
+classifiers = [
+    LogisticRegression(max_iter=1000),
+    LinearSVC(),
+    RandomForestClassifier()
+]
+
+
+
+
+""" for classifier in [LinearSVC(C=99999999999999999999,penalty='l2', dual=False, tol=4)]:
   clf = OneVsRestClassifier(classifier)
   clf.fit(X_train, y_train)
   y_pred = clf.predict(X_test)
   print_score(y_pred, classifier)  
+ """
 
+####################################################STÄLL IN OLIKA PARAMETRAR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!##########################################
+linres=LinearSVC(C=99999999999999999999,penalty='l2', dual=False )
+logres=LogisticRegression()
 
+linres2=LinearSVC(C=99999999999999999999,penalty='l1', dual=False)
+logres2=LogisticRegression(solver="liblinear",multi_class='ovr',penalty='l2',C=1.2)
 
+linres3=LinearSVC(C=99999999999999999999,penalty='l1', dual=False, class_weight="balanced"  )     ##BARA 3 FEL!!!!!!!!
+logres3=LogisticRegression(solver="newton-cg",penalty='l2',C=10)
+
+linres4=LinearSVC(C=99999999999999999999,penalty='l1', dual=False, class_weight="balanced" ,multi_class="ovr" ) ##BARA 3 FEL!!!!!!!!
+logres4=LogisticRegression(solver="newton-cholesky",multi_class='ovr',C=100,class_weight="balanced",penalty="l2")
+
+linres5=LinearSVC(C=99999999999999999999,penalty='l1', dual=False, class_weight="balanced" ,multi_class="crammer_singer")
+logres5=LogisticRegression(solver="liblinear",multi_class='ovr',C=100,class_weight="balanced",dual=False)
+
+#randfor=RandomForestClassifier()
+####LÄGG IN NYA NAMN I LISTAN SÅ TESTAS DESSA
+#classifiers=[linres,logres,linres2,logres2,linres3,logres3,linres4,logres4,linres5,logres5]
+classifiers=[linres4]
 
 
 print("----------------------------testa model----------------------")
 
 antaltitlar=0
 counter=0
+bestScores={}
+round=0
+roundcounter=0
+for classifier in classifiers:
+  
+  if roundcounter%2==0:
+    round=round+1
 
-for titlar in dataframe['Yrkestitel']:
-    antaltitlar=antaltitlar+1
+    print(f"-------------------------------------------RUNDA {round} ------------------------------------------------------")
+  roundcounter=roundcounter+1
+  
 
-    x=[titlar]
+  clf=OneVsRestClassifier(classifier)
+  clf.fit(X_train, y_train)
+  antaltitlar=0
+  counter=0
+  for titlar in dataframe['Yrkestitel']:
+      antaltitlar=antaltitlar+1
 
-    #print(x)
-    #print(multilabel.classes_)
-    xt=tfidf.transform(x)
-    #print(clf.predict(xt))
-    temp=multilabel.inverse_transform(clf.predict(xt))
+      x=[titlar]
 
-    #print(multilabel.inverse_transform(clf.predict(xt)))
-    if len(temp[0])==0:
-       counter=counter+1
-       print(f"Har inte kopplats: {titlar}")
-    else:
-       print(f"Har kopplats: {titlar}")
-    #print("Utskrift------------",clf.predict(xt))
-    #print("_____________________________________________________________________________________________________________________________________")
+      #print(x)
+      #print(multilabel.classes_)
+      xt=tfidf.transform(x)
+      #print(clf.predict(xt))
+      temp=multilabel.inverse_transform(clf.predict(xt))
+
+      #print(multilabel.inverse_transform(clf.predict(xt)))
+      if len(temp[0])==0:
+        counter=counter+1
+        #print(f"Har inte kopplats: {titlar}")
+      else:
+        pass
+        ###För att kontrollera vilket jobb som kopplats till vad avkommentera nedanståend två rader
+        #print(f"Har kopplats: {titlar} till:")
+        #print(f"{temp}\n")
+      
+      
+      #print("Utskrift------------",clf.predict(xt))
+      #print("_____________________________________________________________________________________________________________________________________")
+  print(f"Test av classifire {str(classifier)}")
+  print(f"Antal fel {counter} i procent {(counter/antaltitlar)*100} totalt finns det {antaltitlar} titlar")
+  print("")
+  bestScores[classifier]=counter
+#print(bestScores)
+
+
+x=['Systemutvecklare']
 
 
 print(f"Antal fel {counter} i procent {(counter/antaltitlar)*100} totalt finns det {antaltitlar} titlar")
@@ -152,6 +220,12 @@ xt=tfidf.transform(x)
 #print(clf.predict(xt))
 temp=multilabel.inverse_transform(clf.predict(xt))
 print(temp)
+
+""" for classifier in classifiers:
+    clf = OneVsRestClassifier(classifier)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print(f"Classifier: {classifier.__class__.__name__}, Accuracy: {accuracy_score(y_test, y_pred)}") """
 """ print("-------------------------------------debugg------------------------")
 print("Etikettklasser:", multilabel.classes_)
 target_label = multilabel.classes_[4]
