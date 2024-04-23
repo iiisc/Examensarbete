71ad6d0086942478f5570d046107bc7fe158aa4d
iiisc <63256807+iiisc@users.noreply.github.com>
2024-04-23 11:04:15 +0200

    Adds api calls

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 import os
-import json
 import pandas as pd
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from PyPDF2 import PdfReader
 
 UPLOAD_FOLDER = 'Uploads'
@@ -35,8 +34,6 @@ def upload_file():
 
         return redirect(request.url)
 
-@app.route()
-
 @app.route('/score', methods=['GET'])
 def get_score():
     allCV = { 
@@ -46,7 +43,7 @@ def get_score():
     }
 
     ## Convert nested dict to JSON
-    jsonDict = json.dumps(allCV)
+    jsonDict = jsonify(allCV)
 
     df = pd.DataFrame.from_dict(allCV, orient='index')
     df_html = df.to_html()
@@ -59,5 +56,69 @@ def get_files():
     df_html = df.to_html(index = False)
     return render_template('table.html', table_html = df_html)
 
+@app.route('/api/available_attributes', methods = ['GET'])
+def api_get_attributes():
+    all_attributes = {
+        'Personal Abilities': [
+            'Personlig mognad', 
+            'Integritet', 
+            'Självständighet', 
+            'Initiativtagande', 
+            'Självgående', 
+            'Flexibel', 
+            'Stabil', 
+            'Prestationsorienterad', 
+            'Energisk', 'Uthållig', 
+            'Mål och resultatorienterad'
+        ], 
+        'Social Abilities': [
+            'Samarbetsförmåga', 
+            'Relationsskapande',
+            'Empatisk Förmåga',
+            'Muntlig kommunikation',
+            'Lojal',
+            'Serviceinriktad',
+            'Övertygande',
+            'Kulturell medvetenhet',
+            'Pedagogisk insikt'
+        ],
+        'Leadership Abilities': [
+            'Ledarskap',
+            'Tydlig',
+            'Affärsmässig',
+            'Strategisk',
+            'Omdöme',
+            'Beslutsam'
+        ],
+        'Intellectual Abilities': [
+            'Strukturerad',
+            'Kvalitetsmedveten',
+            'Kreativ',
+            'Specialistkunskap',
+            'Problemlösande Analysförmåga',
+            'Numerisk analytisk förmåga',
+            'Språklig analytisk förmåga'
+        ]
+    }
+    return(jsonify(all_attributes), 200)
+
+@app.route('/api/score', methods = ['GET', 'POST'])
+def api_score_cv():
+    allCV = { 
+        "CarlsCV": {"Score": 10, "PercentScore": 0.3},
+        "ViktorsCV": {"Score": 100, "PercentScore": 0.9},
+        "RandomCV": {"Score": 1, "PercentScore": 0.1}
+    }
+    return jsonify(allCV, 200)
+
+@app.route('/api/upload', methods = ['POST'])
+def api_upload():
+        files = request.files.getlist("")
+        for file in files:   
+            if file and allowed_file(file.filename):
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+          
+        return "hi"
+    
 if __name__ == '__main__':
     app.run()
